3d67e80612b0c227c8f26cfc76169a3453c4f0fa
ReaperC <liucan@LC.local>
2018-01-22 15:51:17 +0800

    新增首页控制器
    新增首页控制器

---
+++ Github_test/MobileFrame/Home/ViewController/MF_HomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  MF_HomeViewController.m
+//  Github_test
+//
+//  Created by 刘灿 on 2018/1/22.
+//Copyright © 2018年 epoint. All rights reserved.
+//
+
+#import "MF_HomeViewController.h"
+
+@interface MF_HomeViewController ()
+
+@end
+
+@implementation MF_HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
