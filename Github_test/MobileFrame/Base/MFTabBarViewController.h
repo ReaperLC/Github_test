d5193a5f837c77263ee185842a968f23aba632df
ReaperC <liucan@LC.local>
2018-01-22 17:06:30 +0800

    根控制器添加
    根控制器添加 1、BaseViewController
                          2、BaseNav
                          3、TabBar

---
+++ Github_test/MobileFrame/Base/MFTabBarViewController.h
@@ -0,0 +1,13 @@
+/**
+ * 作者：刘灿
+ * 创建时间：2018/1/22 17:05
+ * 版本：[1.0, 2018/1/22]
+ * 版权：江苏国泰新点软件有限公司
+ * 描述：<#描述#>
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface MFTabBarViewController : UITabBarController
+
+@end
